3b990f56b8de87ab22ddee2e14cd556a0e95b266
GyanPrakash2483 <gyanprakash2483@gmail.com>
2022-12-11 23:29:21 +0530

    added file copying mechanism

--- src/binrun.c
+++ src/binrun.c
@@ -1,8 +1,25 @@
 //binrun.c
 
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main(int argc, char** argv)
 {
-	printf(%d, &argc);
+
+	int cmd_len = strlen(argv[1]) + 42;
+	char command[cmd_len];
+
+	//initialise variable char array to 0;
+	for(int j = 0; j < cmd_len; j++) {
+	command[j] = 0;
+	}
+
+	strcat(command, "cp ");
+	strcat(command, argv[1]);
+	strcat(command, " /data/data/com.termux/files/home/");
+
+	system(command);
+
+	
 }
